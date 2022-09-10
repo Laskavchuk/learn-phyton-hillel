633ef9a1c0eefa4a9380759833fc2c6a286323d7
Max <maksim.laskavcuk@gmail.com>
2022-09-10 16:12:32 +0300

    Конвертує гривню в долар

--- homework.py
+++ homework.py
@@ -1,4 +1,4 @@
-UAH = int(input('Введіть кількість гривень: '))
+UAH = float(input('Введіть кількість гривень: '))
 DOLLAR = 36.65 #ціна одного долара
 convert = round(UAH / DOLLAR, 2)
 print(convert, '$')
