1054722fdcc587b36bc532468cd5621ced49934f
Max <maksim.laskavcuk@gmail.com>
2022-09-10 16:23:25 +0300

    Конвертує гривню в долар

---
+++ main.py
@@ -0,0 +1,26 @@
+#a = int(input('Введіть число: '))
+#if a % 2 > 0:
+  #  print(a, 'не чьотне число')
+#else:
+ #   print(a, 'чьотне число')
+
+#a = int(input('Введіть число: '))
+#b = int(input('Введіть число: '))
+#if a > b:
+#    c=a-b
+#    print(f'число {a} більше на {c} за число {b}')
+#else:
+#    c=b-a
+#    print(f'число {b} більше на {c} за число {a}')
+'''
+a = int(input('Введіть число: '))
+b = int(input('Введіть число: '))
+if a > b:
+    c = round(a/b, 2)
+    (print(f'число {b} менше {a} у {c} разів'))
+else:
+    c = round(b/a, 2)
+    (print(f'число {a} менше {b} у {c} разів'))
+    '''
+a = float(input('число: '))
+print (a)
