98d9d623516e3007cc6c4e87eea4824833d06b2c
Max <maksim.laskavcuk@gmail.com>
2022-09-22 21:29:06 +0300

    Конвертація доларів у гривню і навпаки

--- homework 3.1.py
+++ homework 3.1.py
@@ -1,11 +1,39 @@
-def convert_dollar_to_uah():
-    if 'Долар' in user.title() or '1' in user or '$' in user:
+def choice():   # Функція зчитує значення ведене користуачем
+    while True:
+        x = input('Виберіть, що будемо коневертувати?: ')
+        if '1' in x or '2' in x or 'Долар' in x.title() or 'Долари' in x.title() or 'Гривня' in x.title() or 'Гривню' \
+                in x.title() or 'Гривні' in x.title() or 'UAH' in x.upper() or '$' in x:
+            return x
+        else:
+            print('Виберіть те, що є в меню ')
+
+
+def change_course():    # Функція зчитує значення ведене користуачем
+    while True:
+        x = input('Бажаєте змінити курс?:  ')
+        if 'Так' in x.title() or 'Ні' in x.title():
+            return x
+        else:
+            print('Введіть "Так" або "Ні"')
+
+
+def course():   # Функція зчитує значення ведене користуачем
+    if 'Так' in user_choice.title():
+        x = float(input('Введіть ціну 1 долара (курс): '))
+    else:
+        x = 36.92
+    return x
+
+
+def convert_dollar_to_uah():    # Функція конвертує долар в гривню
+    if 'Долар' in user.title() or '1' in user or '$' in user or 'Долари' in user.title():
         dollar = float(input("Введіть кількість доларів, яку будемо конвертувати: "))
         return round(course_dollar * dollar, 2)
 
 
-def convert_uah_to_dollar():
-    if 'Гривня' in user.title() or '2' in user or 'UAH' in user.upper() or 'Гривню' in user.title():
+def convert_uah_to_dollar():    # Функція конвертує гривню в долар
+    if 'Гривня' in user.title() or '2' in user or 'UAH' in user.upper() or 'Гривні' in user.title() \
+            or 'Гривню' in user.title():
         uah = float(input("Введіть кількість гривень, яку будемо конвертувати: "))
         return round(uah / course_dollar, 2)
 
@@ -15,16 +43,12 @@ print('''
 1. Долар, $
 2. Гривня, UAH
 ''')
-
-user = input('Виберіть, що будемо коневертувати?: ')
+user = choice()
 print('Ціна 1 долара (курс) за замовчуванням = 36.92 UAH')
-user_choice = input('Бажаєте змінити курс?:  ')
-if 'Так' in user_choice.title():
-    course_dollar = float(input('Введіть ціну 1 долара (курс): '))
-else:
-    course_dollar = 36.92
-
-if 'Гривня' in user.title() or '2' in user or 'UAH' in user.upper() or 'Гривню' in user.title():
+user_choice = change_course()
+course_dollar = course()
+if 'Гривня' in user.title() or '2' in user or 'UAH' in user.upper() or 'Гривню' in user.title() \
+        or 'Гривні' in user.title():
     print(convert_uah_to_dollar(), '$')
 else:
     print(convert_dollar_to_uah(), 'UAH')
