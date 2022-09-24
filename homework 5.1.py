6e4075e9d92289c95b77bf30672e3b5aef525d83
Max <maksim.laskavcuk@gmail.com>
2022-09-24 10:58:47 +0300

    сторони опуклого чотирикутника

---
+++ homework 5.1.py
@@ -0,0 +1,45 @@
+def read_quadrangle_vertice(name):
+    while True:
+        x = input(f'{name} = ')
+        try:
+            x = float(x)
+        except Exception:
+            print('Це не число!')
+            continue
+        if x > 0:
+            return x
+        else:
+            print('Це число не може бути в чотирикутнику!')
+
+
+def does_square_is(a, b, c, d):
+    if a == b and a == c and a == d:
+        return True
+
+
+def does_rectangle_is(a, b, c, d):
+    if pow(a, 2) + pow(b, 2) == pow(c, 2) + pow(d, 2):
+        print("Це є прямокутник ")
+        return True
+    else:
+        print("Це не є прямокутник ")
+        return False
+
+
+def the_area_of_the_rectangle(a, b):
+    return a*b
+print('*' * 32)
+print('Перевірка опуклого чотирикунтка!')
+print('*' * 32)
+
+a = read_quadrangle_vertice('a')
+b = read_quadrangle_vertice('b')
+c = read_quadrangle_vertice('c')
+d = read_quadrangle_vertice('d')
+if does_square_is(a, b, c, d):
+    print('Це квадрат ')
+elif does_rectangle_is(a, b, c, d):
+    print(f'Площа прямокутника = {the_area_of_the_rectangle(a, b)} ')
+
+
+
