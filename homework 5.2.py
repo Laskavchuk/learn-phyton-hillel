6485fc10bb56b6fff03ebabd57a6274dc5e93f23
Max <maksim.laskavcuk@gmail.com>
2022-09-25 12:06:12 +0300

    офіційне письмо

--- homework 5.2.py
+++ homework 5.2.py
@@ -10,25 +10,20 @@ def name_(name):
 
 
 def print_text():
-    x = (f'''
+    y = (f'''
 {'-' * 102}
-  Dear {name_recipient.title()} {last_name_recipient.title()},                                             
+  Dear {name_("Ім'я одержувача").title()} {name_('Прізвище одержувача').title()},                                             
 
 We are lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum in faucibus massa. 
 Suspendisse at ex varius, porttitor eros sit amet, sagittis nibh. In vel est a tortor tempor luctus a. 
    
   ________________
-  {name_sender.title()} {last_name_sender.title()}
-  {position_of_sender.title()}
+  {name_("Ім'я відправика").title()} {name_('Прізвище відправника').title()}
+  {name_('Посаду відправника').title()}
 {'-' * 102} ''')
-    return x
+    return y
 
 print('*' * 27)
 print('Створення офіційного листа!')
 print('*' * 27)
-name_recipient = name_("Ім'я одержувача")
-last_name_recipient = name_('Прізвище одержувача')
-name_sender = name_("Ім'я відправика")
-last_name_sender = name_('Прізвище відправника')
-position_of_sender = name_('Посада відправника')
 print(print_text())
